55b83bb581cc0e00768e50b7060f1b5c347fe11a
skullquake <ockert8080@gmail.com>
2019-09-27 08:10:30 +0200

    .

--- src/btn/a.cpp
+++ src/btn/a.cpp
@@ -2,7 +2,10 @@
 #include<QPushButton>
 int main(int argc,char **argv){
 	QApplication app(argc,argv);
-	QPushButton button("Hello world !");
-	button.show();
+	QWidget window;
+	window.setFixedSize(100, 50);
+	QPushButton *button = new QPushButton("Hello World", &window);
+	button->setGeometry(10, 10, 80, 30);
+	window.show();
 	return app.exec();
 }
